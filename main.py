986d85969dbdb19e68bfa937c7df50d5fba45270
Moein <82890836+MoeinKarimii@users.noreply.github.com>
2021-06-05 13:19:32 +0430

    updated

--- main.py
+++ main.py
@@ -19,24 +19,25 @@ class person:
         self.email = email
         self.phnum = phnum
         #telephon sabet
-        self.cellnum = cellnum
-        #shomare mobil
         self.address = address
 
         if gender not in ['male', 'female', 'else', 'prefer not to say']:
             raise ValueError("you have entered an invalid gender")
         self.__gender = gender
         #jensiyat fard
-        if bday < 1 or bday > 31:
+        if int(bday) < 1 or int(bday) > 31:
             raise ValueError("invalid day")
         self.__bday = bday
         #rooze tavalod (ba tarikh tavalod eshtebah nashavad)
-        if bmonth < 1 or bmonth > 12:
+        if int(bmonth) < 1 or int(bmonth) > 12:
             raise ValueError("invalid month")
         self.__bmonth = bmonth
         #mahe tavalod
-        if byear > today_date.year or byear < 1900:
+        if int(byear) > int(today_date.year) or int(byear) < 1900:
             raise ValueError("the age you entered is not valid")
         self.__byear = byear
         #sale tavalod
-        
+        if num_counter(int(cellnum)) != 11:
+            raise ValueError
+        self.__cellnum = cellnum
+        #shomare mobile
