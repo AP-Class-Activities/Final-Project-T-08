17e0214b089d9c8c8807d0c4a306bde6a52f9e80
Moein <82890836+MoeinKarimii@users.noreply.github.com>
2021-06-05 13:13:44 +0430

    updated

--- main.py
+++ main.py
@@ -1,61 +1,42 @@
 import datetime
 #baraye zaman va tarikhe daqiq
 today_date = datetime.date.today()
+def num_counter(num):
+    i = 0
+    while num > 0:
+        num = num // 10
+        i = i + 1
+    return i
 class person:
-    #az in class baray taerif forooshande va moshtari estefadeh mikonim
-    def __init__(self, fname, lname, ncode, bday, bmonth, byear, email, password, phnum, cellnum, address):
+    #az in class baraye taerif forooshande va moshtari estefadeh mikonim
+    def __init__(self, fname, lname, gender, ncode, bday, bmonth, byear, email, phnum, cellnum, address):
         self.fname = fname
         #first name
         self.lname = lname
         #last name
         self.ncode = ncode
         #code melli
-        self.bday = bday
-        #rooze tavalod (tvajoh konid ke ba tarikh tavalod farq dare)
-        self.bmonth = bmonth
-        #mahe tavalod
-        self.byear = byear
-        #sale tavalod
         self.email = email
-        self.password = password
         self.phnum = phnum
         #telephon sabet
         self.cellnum = cellnum
         #shomare mobil
         self.address = address
-    @property
-    def byear(self):
-        return self.__byear
-    @byear.setter
-    def byear(self, value):
-        if int(1900)<int(value)<int(today_date.year):
-            self.byear = value
-        else:
-            print("Invalid age")
 
-
-
-class kala:
-    #in class baraye estefade dar foroshgah va safhe sefareshat ast
-    def __init__(self , kalname , kalprice , kalcode , kalline , kalscore , kalstock):
-        self.kalname = kalname
-        #name kala
-        self.kalprice = kalprice
-        #gheymat kala
-        self.kalcode = kalcode
-        #code kala dar foroshgah
-        self.kalline = kalline
-        #noe kala (dar dastebandi mahsolat)
-        self.kalscore = kalscore
-        #emteaz kala
-        self.kalstock = kalstock
-        #mojodi kala
-
-
-
-
-
-
-
-
+        if gender not in ['male', 'female', 'else', 'prefer not to say']:
+            raise ValueError("you have entered an invalid gender")
+        self.__gender = gender
+        #jensiyat fard
+        if bday < 1 or bday > 31:
+            raise ValueError("invalid day")
+        self.__bday = bday
+        #rooze tavalod (ba tarikh tavalod eshtebah nashavad)
+        if bmonth < 1 or bmonth > 12:
+            raise ValueError("invalid month")
+        self.__bmonth = bmonth
+        #mahe tavalod
+        if byear > today_date.year or byear < 1900:
+            raise ValueError("the age you entered is not valid")
+        self.__byear = byear
+        #sale tavalod
         
